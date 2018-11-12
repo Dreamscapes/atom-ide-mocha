5968b251a5975de9be005fb73406a1c4f3d9fab5
Robert Rossmann <rr.rossmann@me.com>
2018-11-12 14:42:57 +0100

    chore: fix import name

--- main/index.js
+++ main/index.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const { IDEMocha } = require('atom-ide-mocha-core')
+const { IdeMocha } = require('atom-ide-mocha-core')
 
-module.exports = new IDEMocha()
+module.exports = new IdeMocha()
