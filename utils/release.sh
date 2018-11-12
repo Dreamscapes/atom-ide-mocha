c32eeacacc15e26a6a7a1967f369eb305f16f73a
Robert Rossmann <rr.rossmann@me.com>
2018-11-12 15:55:26 +0100

    chore: fix release script

--- utils/release.sh
+++ utils/release.sh
@@ -3,14 +3,15 @@
 # The dumbest way to tell Atom that we have a new version. APM tries to do way too much for my
 # taste. 🤬
 
-read -r -d BODY <<BODY
+read -r -d '' BODY <<JSON
 {"tag": "${TRAVIS_TAG}"}
-BODY
+JSON
+
+echo "Sending: ${BODY}"
 
 curl -v \
   -H 'Accept: application/json' \
   -H 'Content-Type: application/json' \
   -H "Authorization: ${ATOM_TOKEN}" \
-  -X POST \
   -d "${BODY}" \
   https://atom.io/api/packages/ide-mocha/versions
