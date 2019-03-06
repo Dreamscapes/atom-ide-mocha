b4d03dabf3319c8b157edf862ca6358a18894ea8
Robert Rossmann <rr.rossmann@me.com>
2019-03-06 10:49:15 +0100

    chore: ignore package-lock files

--- utils/version.sh
+++ utils/version.sh
@@ -8,8 +8,7 @@ pkgversion=$(npm info atom-ide-mocha-core dist-tags.latest)
 echo "Preparing version: ${pkgversion}"
 echo
 echo "Installing..."
-npm install --save-exact "atom-ide-mocha-core@${pkgversion}"
-rm package-lock.json
+npm install --save-exact "atom-ide-mocha-core@${pkgversion}" --no-package-lock
 
 # Sync changelog so that it's easily readable from within Atom
 echo "Syncing CHANGELOG.md..."
@@ -19,6 +18,6 @@ echo "Updating package version info..."
 npm version "${pkgversion}" --no-git-tag-version
 
 echo "Committing & tagging..."
-git add -A
+git add CHANGELOG.md package.json
 git commit --message "chore: release ${pkgversion}"
 git tag "v${pkgversion}"
